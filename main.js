5982efe3f2be3e8a82e681f74886d68a59177b2a
Tikhon Belousov <58363010+it1shka@users.noreply.github.com>
2021-10-24 23:09:43 +0300

    bruh

--- main.js
+++ main.js
@@ -2,12 +2,30 @@ window.onload = function() {
 
   document.querySelectorAll('.paragraph').forEach(par => {
     par.addEventListener('click', () => {
-      const inner_content = par.getElementsByTagName('p')
+      const inner_content = par.querySelectorAll('p, img')
       for(const p of inner_content) {
         p.classList.toggle('hidden')
       }
     })
   })
 
+  document.getElementById('alert').addEventListener('click', e => {
+    e.stopImmediatePropagation()
+    alert("Wow! You clicked the button!")
+  })
   
+  let counter = 0
+  const display = document.getElementById('display')
+  document.getElementById('counter').addEventListener('click', e => {
+    e.stopImmediatePropagation()
+    counter++
+    display.innerHTML = `Clicked: ${counter}`
+  })
+
+  document.getElementById('bomb').addEventListener('click', e => {
+    e.stopImmediatePropagation()
+    alert('Boom! What have you done?')
+    e.target.remove()
+  })
+
 }
